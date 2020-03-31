c403d92a66fd795322814476baeb7829d6dfd00b
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-30 21:56:24 -0500

    installed jest and inquirer

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  }
+  
+  const toys = [
+    new Toy("Action Figure", 14.99, 5),
+    new Toy("Rare Toy", 17.99, 1)
+  ];
+  
+  module.exports = {
+    Toy,
+    toys
+  };
+  
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern {
+    constructor(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
+    }
+  }
+  
+  const  = [
+    
+  ];
+  
+  module.exports = {
+  
+  };
+  
