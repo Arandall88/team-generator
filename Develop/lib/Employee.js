3b274e9cf0004cf74d078d8777beb680ed1f371d
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-31 15:02:54 -0500

    added test files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,2 +1,13 @@
 // TODO: Write code to define and export the Employee class
 const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via constructor arguments", () => {
+const name = "Alice";
+const e = new Employee(name);
+expect(e.name).toBe(name);
+});
